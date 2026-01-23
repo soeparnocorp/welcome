a77ab524a966a7442f19379eaad38bf74376195c
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-01-24 04:41:10 +0700

    Refactor service worker code for consistency

--- sw.js
+++ sw.js
@@ -1,18 +1,32 @@
-const CACHE_NAME = 'readtalk-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/vite.svg'
-];
+const CACHE_NAME = "readtalk-cache-v1";
+const urlsToCache = ["/", "/index.html", "/vite.svg"];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
