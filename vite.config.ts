8f626f9613d3b834f2e1baab6794548dbbea18c4
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-01-24 01:29:15 +0700

    Vite configuration for React and PWA
    Configured Vite for React with PWA support and manifest.

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // otomatis update service worker
+      manifest: {
+        name: 'READTalk Messenger',
+        short_name: 'READTalk',
+        description: 'Talk to Trust',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: '/vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ]
+})
