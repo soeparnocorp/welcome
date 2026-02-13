82d63d7ff5dda61dc6e78a1e1ccb749671d8c6c0
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-02-14 04:04:15 +0700

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
   const handleClick = () => {
     // arah ke Pages Function /click
-    window.location.href = '/'
+    window.location.href = '/log/'
   }
 
   return (
