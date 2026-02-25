440e8c01d1e4ab4f10522a4c5077c2fbfc1e8aea
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-02-26 02:27:23 +0700

    auth.ts

--- functions/auth.ts
+++ functions/auth.ts
@@ -1,6 +1,6 @@
 export async function onRequestGet({ env }) {
   
-  const response = await env.OPENAUTH_WORKER.fetch(
+  const response = await env.ACCOUNT_WORKER.fetch(
     new Request("https://internal-placeholder") // placeholder, bypass URL public
   )
 
