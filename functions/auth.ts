d6acff27f3853c2bb856e512a29f053549175d29
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-03-04 01:52:32 +0700

    auth.ts

--- functions/auth.ts
+++ functions/auth.ts
@@ -1,10 +1,14 @@
+// functions/auth.ts
 export const onRequest = async (context) => {
   const { env } = context;
   
   const authorizeUrl = new URL('/authorize', 'https://openauth.soeparnocorp.workers.dev');
-  authorizeUrl.searchParams.set('redirect_uri', 'https://id-readtalk.pages.dev/auth/callback');
+  
+  // Parameter OAuth2 standar
   authorizeUrl.searchParams.set('client_id', 'id-readtalk');
+  authorizeUrl.searchParams.set('redirect_uri', 'https://id-readtalk.pages.dev/callback');
   authorizeUrl.searchParams.set('response_type', 'code');
   
+  // Redirect user ke OpenAuth
   return Response.redirect(authorizeUrl.toString(), 302);
 }
