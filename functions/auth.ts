f37dd152856ae787b231bedd8a340bb9d521cd5b
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-03-04 05:35:35 +0700

    Delete functions/auth.ts

--- functions/auth.ts
+++
@@ -1,7 +0,0 @@
-export const onRequest = async () => {
-  const url = new URL('/authorize', 'https://openauth.soeparnocorp.workers.dev');
-  url.searchParams.set('redirect_uri', 'https://id-readtalk.pages.dev/account');
-  url.searchParams.set('client_id', 'id-readtalk');
-  url.searchParams.set('response_type', 'code');
-  return Response.redirect(url.toString(), 302);
-}
