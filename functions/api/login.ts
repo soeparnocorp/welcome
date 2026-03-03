9353e834c1cf8dc32ee5917871763a8f11ffa986
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-03-04 06:52:58 +0700

    login.ts

--- functions/api/login.ts
+++ functions/api/login.ts
@@ -3,7 +3,7 @@ export async function onRequest(context) {
   const { request } = context;
   const url = new URL(request.url);
 
-  const authUrl = new URL("https://openauth.soeparnocorp.workers.dev/authorize");
+  const authUrl = new URL("https://openauth.soeparnocorp.workers.dev");
   authUrl.searchParams.set("client_id", "id-readtalk");
   authUrl.searchParams.set("response_type", "code");
   authUrl.searchParams.set("redirect_uri", url.origin + "/api/callback");
