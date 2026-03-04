8f3c26370d516b9565ff5ce1d3a1b56d4de028fe
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-03-04 08:10:12 +0700

    Delete functions/api directory

--- functions/api/check-login.ts
+++
@@ -1,7 +0,0 @@
-// functions/api/check-login.ts
-export async function onRequest(context) {
-  const cookie = context.request.headers.get("Cookie") || "";
-  const hasToken = cookie.includes("access_token=");
-
-  return Response.json({ isLoggedIn: hasToken });
-}
