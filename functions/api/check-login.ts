0d055eb0e14d89c11fb729673cf0c5ed579a6b54
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-03-04 08:59:47 +0700

    check-login.ts

--- functions/api/check-login.ts
+++ functions/api/check-login.ts
@@ -1,7 +1,4 @@
 // functions/api/check-login.ts
 export async function onRequest(context) {
-  const cookie = context.request.headers.get("Cookie") || "";
-  const hasToken = cookie.includes("access_token=");
-
-  return Response.json({ isLoggedIn: hasToken });
+  return new Response("OK", { status: 200 });
 }
