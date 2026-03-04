efe3922d104bbc089d93475f9bb9f430a53c84ec
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-03-05 01:19:20 +0700

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
       console.log('Auto-detect: User returning from OpenAuth', { userId, email });
       
       // Redirect ke check-login dengan parameter yang sama
-      window.location.href = `/profile?${window.location.search}`;
+      window.location.href = `/profile${window.location.search}`;
     }
   }, []); // Kosong = jalan sekali pas component mount
 
