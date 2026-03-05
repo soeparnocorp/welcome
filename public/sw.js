ee449600de594999fe13c7c9fcac0bd30ac7de95
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-03-05 23:09:01 +0700

    sw.js

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "id-readtalk-v2.0";
+const urlsToCache = ["/", "/index.html", "/512.svg", "/192.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
