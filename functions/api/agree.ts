c6ccf68ef3c5a992b63a2325cb1eadd8d84e1ffc
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-03-07 08:33:23 +0700

    Delete functions directory

--- functions/api/agree.ts
+++
@@ -1,7 +0,0 @@
-export async function onRequest() {
-  const url = new URL("https://openauth.soeparnocorp.workers.dev/");
-  url.searchParams.set("client_id", "id-readtalk");
-  url.searchParams.set("redirect_uri", "https://id-readtalk.pages.dev/api/callback");
-  url.searchParams.set("response_type", "code");
-  return Response.redirect(url.toString(), 302);
-}
